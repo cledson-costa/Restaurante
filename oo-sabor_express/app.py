411477f58bf5371e2b74d99c8fa0b4acacb7d965
cledson-costa <cledson93@hotmail.com>
2024-02-22 11:46:02 -0300

    atribuindo código

--- oo-sabor_express/app.py
+++ oo-sabor_express/app.py
@@ -4,10 +4,10 @@ from modelos.avaliacao import Avaliacao
 
 restaurante_praca = Restaurante('Peperone','Italiana')
 restaurante_mercado = Restaurante('Bevenuto', 'Fast-Food')
-'''restaurante_mercado.receber_avaliacao('Cledson', 10)
+restaurante_mercado.receber_avaliacao('Cledson', 10)
 restaurante_mercado.receber_avaliacao('Miguel', 8)
 restaurante_mercado.receber_avaliacao('Teresa', 4)
-restaurante_praca.receber_avaliacao('Vanessa', 9)'''
+restaurante_praca.receber_avaliacao('Vanessa', 9)
 restaurante_praca.receber_avaliacao('Yasmin', 4)
 restaurante_praca.receber_avaliacao('Carol', 4)
 
